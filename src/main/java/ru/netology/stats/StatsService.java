f6fec1c0c22f900371d765153282be4ed2c87e84
evgsavelov <savelov08@ya.ru>
2020-08-30 15:06:40 +0300

    update

--- src/main/java/ru/netology/stats/StatsService.java
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -3,7 +3,7 @@ package ru.netology.stats;
 public class StatsService {
     public int calculateSum(int[] month) {
         int sum = 0;
-        for(int x:month){
+        for (int x : month) {
             sum = sum + x;
         }
         return sum;
@@ -11,27 +11,69 @@ public class StatsService {
 
     public int calculateAverageSum(int[] month) {
         int sum = 0;
-        for(int x:month){
+        for (int x : month) {
             sum = sum + x;
         }
-        sum = sum/month.length;
+        sum = sum / month.length;
         return sum;
     }
 
 
-    public long findMax(long[] month){
-        long currentMax = month[0];
-        for(long x:month){
-            if(currentMax<x){
-                currentMax = x;
+    public int findMaxMonth(int[] month) {
+        int monthMax = month[0];
+        int a = 0;
+        int b = 1;
+        for (int x : month) {
+            a = a + 1;
+            if (monthMax <= x) {
+                monthMax = x;
+                b = a;
             }
         }
-        return currentMax;
+        return b;
     }
+
+    public int findMinMonth(int[] month){
+        int monthMin = month[0];
+        int a = 0;
+        int b = 1;
+        for(int x : month){
+            a = a+1;
+            if(monthMin >= x){
+                monthMin = x;
+                b = a;
+            }
+        }
+        return b;
+    }
+
+    public int findQuantityUnderAverage(int[] month){
+        int count = 0;
+        int countUnder = 0;
+        int averageSum = calculateAverageSum(month);
+        for(int x:month){
+            if (averageSum >x){
+                count++;
+                countUnder = count;
+            }
+        }
+        return countUnder;
+    }
+
+    public int findQuantityOverAverage(int[] month){
+        int count = 0;
+        int countOver = 0;
+        int averageSum = calculateAverageSum(month);
+        for(int x:month){
+            if (averageSum < x){
+                count++;
+                countOver = count;
+            }
+        }
+        return countOver;
+    }
+
 }
 
 
-//    Номер месяца, в котором был пик продаж (осуществлены продажи на максимальную сумму)*
-//        Номер месяца, в котором был минимум продаж (осуществлены продажи на минимальную сумму)*
-//        Кол-во месяцев, в которых продажи были ниже среднего (см. п.2)
-//        Кол-во месяцев, в которых продажи были выше среднего (см. п.2)
+
--- src/test/java/ru/netology/stats/StatsServiceTest.java
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -25,11 +25,38 @@ class StatsServiceTest {
 
 
     @Test
-    void findMax(){
+    void findMaxMonth(){
         StatsService service = new StatsService();
-        long[] month = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
-        long expected = 20;
-        long actual = service.findMax(month);
+        int[] month = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 8;
+        int actual = service.findMaxMonth(month);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMinMonth() {
+        StatsService service = new StatsService();
+        int[] month = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 9;
+        int actual = service.findMinMonth(month);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void findQuantityUnderAverage() {
+        StatsService service = new StatsService();
+        int[] month = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 5;
+        int actual = service.findQuantityUnderAverage(month);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void findQuantityOverAverage() {
+        StatsService service = new StatsService();
+        int[] month = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 5;
+        int actual = service.findQuantityOverAverage(month);
         assertEquals(expected, actual);
     }
 }
