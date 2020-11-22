b17c141702700177cd80c50724cd56702ff9de3a
evgsavelov <savelov08@ya.ru>
2020-11-22 22:58:53 +0300

    update 22.11

--- src/main/java/ru/netology/stats/StatsService.java
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -14,37 +14,37 @@ public class StatsService {
         for (int x : month) {
             sum = sum + x;
         }
-        sum = sum / month.length;
+        sum = calculateSum(month) / month.length;
         return sum;
     }
 
 
     public int findMaxMonth(int[] month) {
         int monthMax = month[0];
-        int a = 0;
-        int b = 1;
+        int counterMonth = 0;
+        int returnMonth = 1;
         for (int x : month) {
-            a = a + 1;
+            counterMonth++;
             if (monthMax <= x) {
                 monthMax = x;
-                b = a;
+                returnMonth = counterMonth;
             }
         }
-        return b;
+        return returnMonth;
     }
 
     public int findMinMonth(int[] month){
         int monthMin = month[0];
-        int a = 0;
-        int b = 1;
+        int counterMonth = 0;
+        int returnMonth = 1;
         for(int x : month){
-            a = a+1;
+            counterMonth++;
             if(monthMin >= x){
                 monthMin = x;
-                b = a;
+                returnMonth = counterMonth;
             }
         }
-        return b;
+        return returnMonth;
     }
 
     public int findQuantityUnderAverage(int[] month){
@@ -54,10 +54,10 @@ public class StatsService {
         for(int x:month){
             if (averageSum >x){
                 count++;
-                countUnder = count;
+                //countUnder = count;
             }
         }
-        return countUnder;
+        return count;
     }
 
     public int findQuantityOverAverage(int[] month){
@@ -67,10 +67,10 @@ public class StatsService {
         for(int x:month){
             if (averageSum < x){
                 count++;
-                countOver = count;
+                //countOver = count;
             }
         }
-        return countOver;
+        return count;
     }
 
 }
